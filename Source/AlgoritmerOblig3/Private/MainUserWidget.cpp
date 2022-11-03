94d05062b180004eaaa72f1d0a877f3dd90c54f6
Bjørn Joachim Olsen <jkingolsen@gmail.com>
2022-11-03 17:42:01 +0100

    Widget c++ is added
    Now we can more easily get access to some values to change nodes and such

---
+++ Source/AlgoritmerOblig3/Private/MainUserWidget.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainUserWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Slider.h"
+#include "Components/Button.h"
+#include "../AlgoritmerOblig3GameModeBase.h"
+
+
+bool UMainUserWidget::Initialize() {
+	Super::Initialize();
+
+	if (SpawnNodesButton) {
+		SpawnNodesButton->OnClicked.AddDynamic(this, &UMainUserWidget::ClickSpawnNodes);
+	}
+	if (DeleteNodesButton) {
+		DeleteNodesButton->OnClicked.AddDynamic(this, &UMainUserWidget::ClickDeleteNodes);
+	}
+	if (RunDjikstraButton) {
+		RunDjikstraButton->OnClicked.AddDynamic(this, &UMainUserWidget::ClickDjikstraButton);
+	}
+	if (RunAStarButton) {
+		RunAStarButton->OnClicked.AddDynamic(this, &UMainUserWidget::ClickAStarButton);
+	}
+
+	return true;
+}
+
+void UMainUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime) {
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	NodeAmountText->SetText(FText::FromString(FString::FromInt(NodeSlider->GetValue())));
+}
+
+void UMainUserWidget::ClickSpawnNodes() {
+	UE_LOG(LogTemp, Warning, TEXT("HIT SPAWN NODES BUTTON"));
+}
+
+void UMainUserWidget::ClickDeleteNodes() {
+	UE_LOG(LogTemp, Warning, TEXT("HIT DELETE NODES BUTTON"));
+}
+
+void UMainUserWidget::ClickDjikstraButton() {
+	UE_LOG(LogTemp, Warning, TEXT("HIT DJIKSTRA BUTTON"));
+}
+
+void UMainUserWidget::ClickAStarButton() {
+	UE_LOG(LogTemp, Warning, TEXT("HIT A STAR BUTTON"));
+}
---
+++ Source/AlgoritmerOblig3/Public/MainUserWidget.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALGORITMEROBLIG3_API UMainUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual bool Initialize();
+
+	void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class UButton* SpawnNodesButton;
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class UButton* DeleteNodesButton;
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class USlider* NodeSlider;
+
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class UButton* RunDjikstraButton;
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class UButton* RunAStarButton;
+
+	UPROPERTY(BlueprintReadWrite, Category = "UI", meta = (BindWidget))
+		class UTextBlock* NodeAmountText;
+
+	UFUNCTION()
+	void ClickSpawnNodes();
+	UFUNCTION()
+	void ClickDeleteNodes();
+	UFUNCTION()
+	void ClickDjikstraButton();
+	UFUNCTION()
+	void ClickAStarButton();
+};
